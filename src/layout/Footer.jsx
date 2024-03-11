38ad929c52d05620c826ccc77f3a073287b3b7c5
Emil Blichfeldt <eb@limecode.dk>
2024-03-11 13:47:28 +0100

    commit "update"

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,8 +1,104 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import useRequestData from '../hooks/useRequestData'
+
 
 const Footer = () => {
+  const { data, isLoading, error, makeRequest } = useRequestData();
+  const date = new Date();
+  const year = date.getFullYear();
+
+  useEffect(() => {
+
+    makeRequest("http://localhost:5333/contactinformation")
+
+  }, [])
+
+  const myMenu = [
+    {
+      "title": "> Foride",
+      "link": "/"
+    },
+    {
+      "title": "> Om os",
+      "link": "/omos"
+    },
+    {
+      "title": "> Service",
+      "link": "/service"
+    },
+    {
+      "title": "> FAQ",
+      "link": "/faq"
+    },
+    {
+      "title": "> Nyheder",
+      "link": "/nyheder"
+    },
+    {
+      "title": "> Kontakt os",
+      "link": "/kontakt"
+    }
+  ]
+
   return (
-    <div>Footer</div>
+    <>
+      <footer className='footerContainer'>
+        <div className="footerContent">
+
+          <div>
+            <img src="/public/asssets/logo.png" alt="" />
+            <p>Som medlem af Elinstallatørernes Landsorganisation, ELFO, er vi tilsuttet et ankernævn og en garantiordning</p>
+          </div>
+          <div>
+            <h3>Link</h3>
+            {myMenu.map((item, index) => (
+              <ul key={index}>
+                <li>
+                  <Link to={item.title}>
+                    {item.title}
+                  </Link>
+                </li>
+              </ul>
+            ))}
+          </div>
+          <div>
+            <h3>Kontakt os</h3>
+            <p>Adresse: {data?.adress}</p>
+            <p>Telefon:
+              <Link to={'tel:' + data?.phone}>
+                {data?.phone}
+              </Link></p>
+            <p>Mail: <Link to={'mailto:' + data?.email}>
+              {data?.email}
+            </Link></p>
+          </div>
+          <div>
+            <h3>Nyhedsbrev</h3>
+            <p>Tilmeld dig vores nyhedsbrev her</p>
+            <form action="">
+              <input type="email" name="newsSubscription" required />
+              <button type="submit" className='btn'>Tilmeld</button>
+            </form>
+          </div>
+
+
+          <div className='footerCopyrigt'>
+            <p>
+              <span className='textOrange'>Strøm</span>
+              &copy; {year} 2017 All Right Reserved
+            </p>
+          </div>
+
+          <div className='footerSocial'>
+            {data?.some.map((item, index) => (
+              <i>{item.icon}</i>
+            ))}
+          </div>
+        </div>
+      </footer >
+
+    </>
   )
 }
 
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import useRequestData from '../hooks/useRequestData'
+import { MdPlace, MdOutlineAccessTime, MdLocalPhone } from "react-icons/md";
 
 const Header = () => {
+  const { data, isLoading, error, makeRequest } = useRequestData();
+
+  useEffect(() => {
+
+    makeRequest("http://localhost:5333/contactinformation")
+
+  }, [])
+
+  const myMenu = [
+    {
+      "title": "Foride",
+      "link": "/"
+    },
+    {
+      "title": "Om os",
+      "link": "/omos"
+    },
+    {
+      "title": "Service",
+      "link": "/service"
+    },
+    {
+      "title": "FAQ",
+      "link": "/faq"
+    },
+    {
+      "title": "Nyheder",
+      "link": "/nyheder"
+    },
+    {
+      "title": "Kontakt os",
+      "link": "/kontakt"
+    }
+  ]
+
   return (
-    <div>Header</div>
+    <>
+      <header className='headerContainer'>
+        <div className='headerTopbar'>
+          <div className="logo">
+            <img src="/public/asssets/logo.png" alt="" />
+          </div>
+          <div className='contact'>
+            <p><span className='contactIcon'><MdPlace /></span>{data?.address}</p>
+            <p><span className="contactIcon"><MdOutlineAccessTime /></span>{data?.openinghours}</p>
+            <Link to={'tel:' + data?.phone}>
+              <span className="contactIcon"><MdLocalPhone /></span>{data?.phone}
+            </Link>
+          </div>
+        </div>
+        <nav className='navBar'>
+          {myMenu.map((item, index) => (
+            <ul key={index}>
+              <li>
+                <Link to={item.link}>
+                  {item.title}
+                </Link>
+              </li>
+            </ul>
+          ))}
+          <div>
+            <input type="text" name="search" placeholder='Søg' />
+          </div>
+        </nav>
+      </header>
+
+    </>
   )
 }
 
