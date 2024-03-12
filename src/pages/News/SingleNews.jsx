63c45a0f574b31f6430be7ec1706bb9436b05b03
Emil Blichfeldt <eb@limecode.dk>
2024-03-12 15:06:50 +0100

    commit "update"

--- src/pages/News/SingleNews.jsx
+++ src/pages/News/SingleNews.jsx
@@ -1,18 +1,44 @@
-import React, { useEffect } from 'react'
-import { useParams, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams, useLocation, Link } from 'react-router-dom';
 import PageHeader from '../../components/PageHeader';
 import useRequestData from '../../hooks/useRequestData';
 import { FaRegComments, FaCalendarAlt } from 'react-icons/fa';
 
 const SingleNews = () => {
-  const { data, isLoading, error, makeRequest } = useRequestData()
-  const { id } = useParams()
+  const { data, isLoading, error, makeRequest } = useRequestData();
+  const { data: dataAll, isLoading: isLoadingAll, error: errorAll, makeRequest: makeRequestAll } = useRequestData()
+  const { id } = useParams();
   const pathnames = useLocation().pathname.split('/').filter((x) => x);
+  const [formattedDate, setFormattedDate] = useState('');
+  const [formattedComments, setFormattedComments] = useState([]);
 
   useEffect(() => {
     makeRequest("http://localhost:5333/news/" + id);
+    makeRequestAll("http://localhost:5333/news/");
   }, []);
 
+
+  useEffect(() => {
+    const formatReceivedDate = (dateString) => {
+      const options = { day: '2-digit', month: 'short', year: 'numeric' };
+      return new Date(dateString).toLocaleDateString('da-DK', options);
+    };
+
+    if (data && data.received) {
+      const formatted = formatReceivedDate(data.received);
+      setFormattedDate(formatted);
+    }
+
+    if (data && data.comments) {
+      const formattedComments = data.comments.map((comment) => {
+        return {
+          ...comment,
+          received: formatReceivedDate(comment.received)
+        };
+      });
+      setFormattedComments(formattedComments);
+    }
+  }, [data]);
 
   return (
     <section className='singleNewsContainer'>
@@ -25,7 +51,7 @@ const SingleNews = () => {
             <div className="SingleNewshead">
               <img src={`http://localhost:5333/images/news/${data?.image}`} alt="" />
               <div className="newsDate">
-                <p>{data?.received}</p>
+                <p>{formattedDate}</p>
               </div>
             </div>
             <div className="singleNewsBody">
@@ -43,7 +69,7 @@ const SingleNews = () => {
           {/* Kommentar */}
           <div className="commentsField">
             <h2>Kommentar(2)</h2>
-            {data?.comments.map((item, index) => (
+            {formattedComments.map((item, index) => (
               <div className="comments" key={index}>
                 <h3>{item.name}</h3>
                 <p><FaCalendarAlt className='newsArchiveIcon' /> {item.received}</p>
@@ -64,12 +90,22 @@ const SingleNews = () => {
         <aside className="newsArchive">
           <h3>Arkiv:</h3>
           <article className="newsArchiveCard">
-            content
+            {dataAll
+              ?.slice(4, 8).map((item, index) => (
+                <div key={index}>
+                  <Link to={'/'}>
+                    <img src={`http://localhost:5333/images/news/${item.image}`} alt="" />
+                    <p>{item.content.substring(0, 50)}...</p>
+                    <p><FaCalendarAlt className='newsArchiveIcon' /> {item.received}</p>
+                  </Link>
+                  <div className="archiveCardLine"></div>
+                </div>
+              ))}
           </article>
         </aside>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default SingleNews
+export default SingleNews;
