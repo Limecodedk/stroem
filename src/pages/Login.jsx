d0736d343efc9dbdb679e62da41a494a53e9e7c6
Emil Blichfeldt <eb@limecode.dk>
2024-03-13 12:09:36 +0100

    commit "update"

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 const Login = () => {
+  const navigate = useNavigate();
+
+
+  const handleLogin = () => {
+    navigate("/admin/")
+  }
+
+
   return (
-    <div>Login</div>
+    <section className='loginContainer'>
+      <div className="headingContainer">
+        <div className="heading">
+          <h1>Login</h1>
+        </div>
+      </div>
+      <div className='loginFormContainer'>
+        <h2>Logind</h2>
+        <form className="loginForm" onSubmit={handleLogin}>
+          <input type="email" name="email" placeholder='Email' />
+          <input type="password" name="password" placeholder='Adgangskode' />
+          <button type='submit' className='btn effect2'>Login</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
